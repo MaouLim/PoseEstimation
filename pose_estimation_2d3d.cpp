6d86e3a9ed88b6b391248dcec141cb7566a67382
MaouLim <502329767@qq.com>
2019-12-19 10:18:38 +0800

    modified pose_estimation_2d3d.cpp

--- pose_estimation_2d3d.cpp
+++ pose_estimation_2d3d.cpp
@@ -209,7 +209,7 @@ int main(int argc, char** argv) {
 		std::cout << "time used: "
 		          << chrono::duration_cast<chrono::milliseconds>(end - start).count()
 		          << std::endl;
-	 }
+	}
 
 	return 0;
 }
